b23ad7dd9f92c7a8e812f38140179294a859c8bf
Avik Nandi <aviknandi@yahoo.com>
2018-06-19 13:00:57 +0530

    Removed unnessary file and added some important file

--- app/helper/catchBlankString.py
+++
@@ -1,5 +0,0 @@
-def catchEmptyString(blank_string_checking, msg):
-    for pos,value in enumerate(blank_string_checking):
-        print(pos,value)
-        if not value:
-            return {"message":msg[pos]+" Cannot Be Blank"}
